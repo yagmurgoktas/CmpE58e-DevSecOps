9978f46710280de5e2ea9277cd62852502458335
Yagmur Goktas <yagmur.goktas@bitbucket.com>
2025-05-25 21:50:17 +0300

    exclude from coverage

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 """ Flask application entry point """
 
-from app import app
+from app import app # pragma: no cover
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__": # pragma: no cover
+    app.run(host="0.0.0.0", port=5000) # pragma: no cover
