fdc397e50e74a249e341864b54c801d8aeb7bd41
Yagmur Goktas <yagmur.goktas@bitbucket.com>
2025-05-25 21:41:34 +0300

    no cover

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
+# pragma: no cover
 """ Flask application entry point """
 
-from app import app # pragma: no cover
+from app import app
 
-if __name__ == "__main__":  # pragma: no cover
-    app.run(host="0.0.0.0", port=5000) # pragma: no cover
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
